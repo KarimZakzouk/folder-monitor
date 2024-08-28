d73b7a40777c4d3e755bbf0b5055cddafb69b6cd
KarimZakzouk <kokyfarid82@yahoo.com>
2024-08-28 07:21:22 +0300

    added test files

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 const Folder = 'Folder'
+
+fs.readdir(Folder, (err, files) => {
+    files.forEach(file => {
+        console.log(`${file}`);
+    });
+});
 
 fs.watch(Folder, (event, file) => {
     const date = new Date();
